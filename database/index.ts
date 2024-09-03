90d6a100de29cdd4d8266e9b62f915cb2e374662
exa31 <bloodsuker18@gmail.com>
2024-09-03 14:45:45 +0700

    change db with atlas

--- database/index.ts
+++ database/index.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://localhost:27017/iphonestore').then(() => {
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.ATLAS_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=AtlasCluster`).then(() => {
     console.log('Connected to MongoDB');
 });
 
--- dist/database/index.js
+++ dist/database/index.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-mongoose_1.default.connect('mongodb://localhost:27017/iphonestore').then(() => {
+mongoose_1.default.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.ATLAS_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=AtlasCluster`).then(() => {
     console.log('Connected to MongoDB');
 });
 const db = mongoose_1.default.connection;
