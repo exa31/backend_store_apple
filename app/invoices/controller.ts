c87a34da49010a4a9ed5c237f1139fbe9a30a070
exa31 <bloodsuker18@gmail.com>
2024-10-20 02:24:49 +0700

    update getInvoice select user no password dll

--- app/invoices/controller.ts
+++ app/invoices/controller.ts
@@ -6,9 +6,9 @@ export const getInvoice = async (req: Request, res: Response, next: NextFunction
     try {
         const invoices: Invoice | null = await Invoices.findOne({ order: req.params.orderId }).populate({
             path: 'user',
-            select: '-password -token -createdAt -updatedAt -role -cart -likes -_id -__v'
+            select: '-password -token -createdAt -updatedAt -role -cart -likes  -__v'
         }).populate('order');
-        checkIsUserData(invoices!.user._id.toString()!);
+        checkIsUserData(invoices!.user._id.toString());
         res.status(200).json(invoices);
     } catch (error) {
         next((error as Error));
